7441d940d953866560b6d196f331d8fdd31e1faf
Andrew Fraser <arf9999+gh@gmail.com>
2021-02-10 18:49:24 +0200

    reduce pause to 30s

--- R/archive.R
+++ R/archive.R
@@ -87,6 +87,6 @@ web_archive_list <- function(url_list, cookies) {
       ifelse((steps + 4) > length(url_list), length(url_list), steps + 4)
 
     purrr::map(url_list[start:end], pass_query, cookies = cookies)
-    Sys.sleep(45)
+    Sys.sleep(30)
   }
 }
